353c64d84027ca9bbbc38ccee5a16d5a12c73f20
Arthur Araújo <webarthur@gmail.com>
2016-12-01 13:00:21 -0200

    Update Element.hide.js

--- src/Element.hide.js
+++ src/Element.hide.js
@@ -2,3 +2,8 @@ Element.prototype.hide = function () {
 	this.style.display = 'none';
 	return this;
 };
+NodeList.prototype.hide = function () {
+	for(var i=0; i<this.length; i++) {
+		this[i].hide();
+	}
+};
