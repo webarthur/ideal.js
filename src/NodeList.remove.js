c975c71c9c27fe5506b7066dfaf7889e37b87532
Arthur Araújo <webarthur@gmail.com>
2016-12-01 13:10:49 -0200

    Delete NodeList.remove.js

--- src/NodeList.remove.js
+++
@@ -1,7 +0,0 @@
-NodeList.prototype.remove = HTMLCollection.prototype.remove = function() {
-    for(var i = this.length - 1; i >= 0; i--) {
-        if(this[i] && this[i].parentElement) {
-            this[i].parentElement.removeChild(this[i]);
-        }
-    }
-};
