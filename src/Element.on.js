cc055708929ae460440e37efb90afc8c00dc915f
Arthur Araújo <webarthur@gmail.com>
2016-12-01 12:57:53 -0200

    Create Element.on.js

---
+++ src/Element.on.js
@@ -0,0 +1,7 @@
+Element.prototype.on = function(evt, fn) {
+	if (this.addEventListener)
+    this.addEventListener(evt, fn, false);
+	else if (this.attachEvent)
+    this.attachEvent('on' + evt, fn);
+	return this;
+};
