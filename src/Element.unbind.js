3bc5e5d79f0e6833dfdb015327cf60aabd96255d
Arthur Araújo <webarthur@gmail.com>
2016-12-01 12:51:04 -0200

    Create Element.unbind.js

---
+++ src/Element.unbind.js
@@ -0,0 +1,4 @@
+// http://stackoverflow.com/questions/3387427/remove-element-by-id
+Element.prototype.unbind = function(type, handle) {
+	document.removeEventListener? this.removeEventListener( type, handle, false ) : this.detachEvent( "on" + type, handle );
+};
