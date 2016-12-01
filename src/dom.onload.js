1a2d908b59a6d0f1543355752cf99592dfbb7db2
Arthur Araújo <webarthur@gmail.com>
2016-12-01 14:30:46 -0200

    Create dom.onload.js

---
+++ src/dom.onload.js
@@ -0,0 +1,15 @@
+/**
+ * Class to add dom load functions and execute all node with onload attribute.
+ *
+ * @author Arthur Araújo
+ * @date 01/12/2016
+ */
+dom.onload = function (func) {
+	return window.addEventListener('load', func);
+};
+dom.onload(function () {
+	dom.querySelectorAll('[onload]').forEach(function (node) {
+		var self = node;
+		eval(self.getAttribute('onload'));
+	});
+});
