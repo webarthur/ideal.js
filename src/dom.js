5c7ef5e9471846e2973e1723c4e8e30d462c03e7
Arthur Araújo <webarthur@gmail.com>
2016-12-01 12:50:33 -0200

    Create dom.js

---
+++ src/dom.js
@@ -0,0 +1,12 @@
+/**
+ * Class to manipulate DOM.
+ *
+ * @author Arthur Araújo
+ * @date 01/12/2016
+ */
+var dom = document.childNodes[1];
+dom.body = dom.querySelector('body');
+dom.head = dom.querySelector('head');
+dom.element = function (id) {
+	return document.getElementById(id);
+};
