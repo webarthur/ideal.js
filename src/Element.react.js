87768afd8392dea82d8b75777edbd3dc7cf7736c
Arthur Araújo <webarthur@gmail.com>
2016-12-01 12:54:10 -0200

    Create Element.react.js

---
+++ src/Element.react.js
@@ -0,0 +1,19 @@
+/**
+ * Class to manipulate DOM.
+ *
+ * @author Arthur Araújo
+ * @date 01/12/2016
+ */
+Element.prototype.react = function(data) {
+	var nodeList = this.querySelectorAll('[data-react]');
+	if(nodeList) {
+		if(this.getAttribute('data-react')) {
+			var self = this;
+			eval(self.getAttribute('data-react'));
+		}
+		nodeList.forEach(function (self) {
+			eval(self.getAttribute('data-react'));
+		});
+	}
+	return this;
+};
