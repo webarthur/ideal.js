923c558117f8d8dac551186194abede557cbafc0
Arthur Araújo <webarthur@gmail.com>
2016-12-27 10:31:05 -0200

    Create String.stripTags.js

---
+++ src/String.stripTags.js
@@ -0,0 +1,10 @@
+String.prototype.stripTags = function (allowed) {
+  allowed = (((allowed || '') + '').toLowerCase().match(/<[a-z][a-z0-9]*>/g) || []).join('');
+
+  var tags = /<\/?([a-z][a-z0-9]*)\b[^>]*>/gi;
+  var commentsAndPhpTags = /<!--[\s\S]*?-->|<\?(?:php)?[\s\S]*?\?>/gi;
+
+  return this.replace(commentsAndPhpTags, '').replace(tags, function ($0, $1) {
+    return allowed.indexOf('<' + $1.toLowerCase() + '>') > -1 ? $0 : ''
+  })
+};
