85d0245d088cd46677e7f7de623f87197161da56
Arthur Araújo <webarthur@gmail.com>
2017-02-22 22:16:16 -0300

    Create location.getQueryParams.js

---
+++ src/location.getQueryParams.js
@@ -0,0 +1,38 @@
+window.location.getQueryParams = function(query) {
+  
+  if (!query) {
+    query = window.location.search.substring(1)
+  }
+  
+  var l = query.length
+  var q = {} // query
+  var n = '' // name
+  var v = '' // value
+  var t = false
+  
+  for(i=0; i<l; i++) {
+    var c = query[i]
+    
+    if (c==='=') {
+      t = true
+    }
+    else if (c==='&' || i===l-1) {
+      t = false
+      q[n] = decodeURIComponent(v)
+      n = ''
+      v = ''
+    }
+    else if (i>0 || c!=='?') {
+      if (t) {
+        if (c==='+') {
+          c = ' '
+        }
+        v += c
+      }
+      else {
+        n += c
+      }
+    }
+  }
+  return q
+}
