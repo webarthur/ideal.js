41428b8b9dc7e2e13f28c239cc66a60accb42098
Arthur Araújo <webarthur@gmail.com>
2017-03-06 14:04:38 -0300

    Create Array.remove.js

---
+++ src/Array.remove.js
@@ -0,0 +1,13 @@
+Array.prototype.remove = function (item, _delete) {
+    var i = this.indexOf(item)
+    var arr = this
+    if (i>-1) {
+      if (_delete) {
+        delete arr[i]
+      }
+      else {
+        arr = arr.splice(i, 1)
+      }
+    }
+    return arr
+}
