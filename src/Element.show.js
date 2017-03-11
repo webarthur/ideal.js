a9210e4c0d8ed4763ea28aeed1b27db671d74e10
Arthur Araújo <webarthur@gmail.com>
2017-03-11 14:42:20 -0300

    Element.show + Element.hide fix

--- src/Element.show.js
+++ src/Element.show.js
@@ -1,9 +1,10 @@
 $E.hide = function () {
-	this.style.display = 'none'
+	this.style.display = ''
+	this.removeAttribute('hidden')
 	return this
 }
 $N.hide = function () {
 	const l = this.length
-	for(let i=0; i<l; this[i++].hide());
+	for(let i=0; i<l; this[i++].show());
 	return this
 }
